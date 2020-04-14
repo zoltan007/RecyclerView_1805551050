76e3b65dbc32baff5d57192f451e254963b8e03b
Your Name <you@example.com>
2020-04-14 18:36:39 +0800

    first upload

---
+++ app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.android.recyclerview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.LinkedList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                mWordList.addLast("+ Word " + wordListSize);
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/android/recyclerview/WordListAdapter.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            int mPosition = getLayoutPosition();
+
+
+            String element = mWordList.get(mPosition);
+
+
+            mWordList.set(mPosition, "Clicked! " + element);
+
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    /**
+     * Called when RecyclerView needs a new ViewHolder of the given type to
+     * represent an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can
+     * represent the items of the given type. You can either create a new View
+     * manually or inflate it from an XML layout file.
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * onBindViewHolder(ViewHolder, int, List). Since it will be reused to
+     * display different items in the data set, it is a good idea to cache
+     * references to sub views of the View to avoid unnecessary findViewById()
+     * calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after
+     *                 it is bound to an adapter position.
+     * @param viewType The view type of the new View. @return A new ViewHolder
+     *                 that holds a View of the given view type.
+     */
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        // Inflate an item view.
+        View mItemView = mInflater.inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+
+    @Override
+    public void onBindViewHolder(WordListAdapter.WordViewHolder holder,
+                                 int position) {
+
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
